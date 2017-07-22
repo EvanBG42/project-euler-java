72c5c49e0e75ae40abaf9167f9dbe29d463f53ec
EvanBG42 <Evanbg42@gmail.com>
2017-07-21 22:23:21 -0400

    oi

---
+++ src/problems1to50/Problem3.java
@@ -0,0 +1,16 @@
+/*
+ * Problem 3: 
+ * The prime factors of 13195 are 5, 7, 13 and 29.
+ * What is the largest prime factor of the number 600851475143 ?
+ */
+
+package problems1to50;
+
+public class Problem3 {
+
+	public static void main(String[] args) {
+		// Prime factorization is tough.
+
+	}
+
+}

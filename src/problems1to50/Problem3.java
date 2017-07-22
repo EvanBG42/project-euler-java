d95d3f9364c302b3a80ea3ddb17590c542761ceb
EvanBG42 <Evanbg42@gmail.com>
2017-07-22 11:49:29 -0400

    starting prob 4

--- src/problems1to50/Problem3.java
+++ src/problems1to50/Problem3.java
@@ -5,12 +5,40 @@
  */
 
 package problems1to50;
+import java.util.*;
+import java.lang.Math;
 
 public class Problem3 {
 
 	public static void main(String[] args) {
 		// Prime factorization is tough.
+		System.out.println(primeFactorize(600851475143L));
 
+	}
+	
+	// Method to return prime factors
+	public static ArrayList<Long> primeFactorize(long n){
+		ArrayList<Long> primeFactors = new ArrayList<Long>();
+		
+		
+		while (n % 2 == 0){
+			primeFactors.add((long) 2);
+			n /= 2;
+		}
+		
+		// This bit is really interesting
+		for (long i = 3; i < Math.sqrt(n); i += 2){
+			if (n % i == 0){
+				primeFactors.add(i);
+				n /= i;
+			}
+		}
+		
+		if (n > 2) {
+			primeFactors.add(n);
+		}
+		
+		return primeFactors;
 	}
 
 }

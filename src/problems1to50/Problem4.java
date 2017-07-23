111e0947bdcc35493e6378d4478a1b0d146d920a
EvanBG42 <Evanbg42@gmail.com>
2017-07-23 12:41:58 -0400

    palindrome checker is done

--- src/problems1to50/Problem4.java
+++ src/problems1to50/Problem4.java
@@ -7,23 +7,26 @@
 
 package problems1to50;
 
+import java.util.Arrays;
+
 public class Problem4 {
 
 	public static void main(String[] args) {
 		// Largest product of two 3-digit nums is 6 digits
 		// 999 * 999 == 998001
-		System.out.println(isNumPalindrome(3002));
+		System.out.println(isNumPalindrome(1234));
 
 	}
 	
 	public static boolean isNumPalindrome(int n){
 		boolean palindrome = true;
-		String nStr = Integer.toString(n);
+		char[] nChars = Integer.toString(n).toCharArray();
 		
-		// FIXME: palindrome checker
-		for (int i = nStr.length()-1; i <= 0; i--){
-			if (nStr.charAt(i) != nStr.charAt(i - nStr.length())){
+		// Palindrome checker
+		for (int i = 0; i < nChars.length; i++){
+			if (nChars[i] != nChars[nChars.length - (i+1)]){
 				palindrome = false;
+				break;
 			}
 		}
 		return palindrome;
